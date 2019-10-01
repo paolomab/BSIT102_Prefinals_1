840c23e13f48a77427da3d8279805851e620cf21
paolomab <53303346+paolomab@users.noreply.github.com>
2019-10-01 09:56:09 +0800

    Update PrefinActivity1.java

--- PrefinActivity1.java
+++ PrefinActivity1.java
@@ -4,7 +4,7 @@ public class PrefinActivity1 {
 
         public static void main(String[] args) {
             int number = 0;
-            Scanner scanner = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in); //The use of Scanner//
             System.out.print("Please type a number between 0 and 1000 OR type -1 to exit:  ");
             number = scanner.nextInt(); //Here, number is the variable//
             while(number!=-1){
@@ -13,8 +13,8 @@ public class PrefinActivity1 {
                         System.out.print("Converted in to Words:\tZERO");
                     } else {
                         System.out.print("Converted in to Words:\t"); //This will be input after entering number from 0 - 1000//
-                        numberToWord(((number / 100) % 10), " Hundred");
-                        numberToWord(((number / 1000) % 100), " Thousand");
+                        numberToWord(((number / 100) % 10), " Hundred"); //This is for Hundreds//
+                        numberToWord(((number / 1000) % 100), " Thousand"); //This is for One Thousand//
                         numberToWord((number % 100), " ");
                     }
 
@@ -28,8 +28,8 @@ public class PrefinActivity1 {
 
         public static void numberToWord(int num, String val) {
             String ones[] = {" ", " One", " Two", " Three", " Four", " Five", " Six", " Seven", " Eight", " Nine", " Ten", " Eleven", " Twelve", " Thirteen", " Fourteen", " Fifteen", " Sixteen", " Seventeen", " Eighteen", " Nineteen"
-            };
-            String tens[] = {" ", " ", " Twenty", " Thirty", " Fourty", " Fifty", " Sixty", " Seventy", " Eighty", " Ninety"};
+            }; //This is the Conversion to Ones//
+            String tens[] = {" ", " ", " Twenty", " Thirty", " Fourty", " Fifty", " Sixty", " Seventy", " Eighty", " Ninety"}; //This is the Conversion to Tens//
             if (num > 19) {
                 System.out.print(tens[num / 10] + " " + ones[num % 10]);
             } else {
